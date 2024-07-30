445002e87004d6a64d01b276bf85efebc3e27e38
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-07-30 21:22:00 +0530

    Implemented captcha for buyer login

--- frontend/src/components/LoginBuyer.js
+++ frontend/src/components/LoginBuyer.js
@@ -1,84 +1,149 @@
 import axios from "axios";
-import { useState } from "react";
-import Button from 'react-bootstrap/Button';
+import { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  LoadCanvasTemplateNoReload,
+  validateCaptcha,
+} from "react-simple-captcha";
 
-export default function LoginBuyer(){
+export default function LoginBuyer() {
+  if (sessionStorage.getItem("sAyurCenReyub") !== null) {
+    window.location.replace("/buyerhome");
+  }
 
-    if(sessionStorage.getItem("sAyurCenReyub") !== null){
-        window.location.replace("/buyerhome");
-    }
+  const [email, setEmail] = useState({});
+  const [password, setPassword] = useState({});
+  const [captchaText, setCaptchaText] = useState("");
 
-    const [email, setEmail] = useState({});
-    const [password, setPassword] = useState({});
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
 
-    function validate(e){
-        e.preventDefault();
-        axios.get(`http://localhost:8070/buyer/get/email/${email}`).then((res)=>{
-            if (res.data[0].password === password){
-                sessionStorage.setItem("sAyurCenReyub", Math.random().toString());
-                sessionStorage.setItem("buyerEmail", email);
-                window.location.replace(`http://localhost:3000/buyerhome`);
-            }
-            else{
-                alert("Invalid Credentials !");
-            }
-        }).catch((err)=>{
-            alert("Please register your account !");
+  function validate(e) {
+    e.preventDefault();
+    if (validateCaptcha(captchaText) === true) {
+      axios
+        .get(`http://localhost:8070/buyer/get/email/${email}`)
+        .then((res) => {
+          if (res.data[0].password === password) {
+            sessionStorage.setItem("sAyurCenReyub", Math.random().toString());
+            sessionStorage.setItem("buyerEmail", email);
+            window.location.replace(`http://localhost:3000/buyerhome`);
+          } else {
+            alert("Invalid Credentials !");
+            loadCaptchaEnginge(6);
+            setCaptchaText("");
+          }
         })
+        .catch((err) => {
+          alert("Please register your account !");
+          loadCaptchaEnginge(6);
+          setCaptchaText("");
+        });
+    } else {
+      alert("Captcha does not match!");
+      loadCaptchaEnginge(6);
+      setCaptchaText("");
     }
+  }
 
-    return(
-        
-        <div className="container">
-            <a href="/"><Button variant="dark">Back</Button></a>
-            <form onSubmit={validate}>
-                <section className="vh-100 gradient-custom">
-                    <div className="container py-5 h-100">
-                        <div className="row d-flex justify-content-center align-items-center h-100">
-                            <div className="col-12 col-md-8 col-lg-6 col-xl-5">
-                                <div className="card bg-dark text-white" style={{borderRadius: "1rem"}}>
-                                    <div className="card-body p-5 text-center">
+  return (
+    <div className="container">
+      <a href="/">
+        <Button variant="dark">Back</Button>
+      </a>
+      <form onSubmit={validate}>
+        <section className="vh-100 gradient-custom">
+          <div className="container py-5 h-100">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+                <div
+                  className="card bg-dark text-white"
+                  style={{ borderRadius: "1rem" }}
+                >
+                  <div className="card-body p-5 text-center">
+                    <div className="mb-md-5 mt-md-4 pb-5">
+                      <h2 className="fw-bold mb-2 text-uppercase">
+                        Buyer Login
+                      </h2>
+                      <p className="text-white-50 mb-5">
+                        Please enter your email and password!
+                      </p>
 
-                                        <div className="mb-md-5 mt-md-4 pb-5">
+                      <div className="form-outline form-white mb-4">
+                        <input
+                          type="email"
+                          id="email"
+                          className="form-control form-control-lg"
+                          placeholder="abc@gmail.com"
+                          required
+                          onChange={(e) => {
+                            setEmail(e.target.value);
+                          }}
+                        />
+                        <label className="form-label" htmlFor="typeEmailX">
+                          Email
+                        </label>
+                      </div>
 
-                                            <h2 className="fw-bold mb-2 text-uppercase">Buyer Login</h2>
-                                            <p className="text-white-50 mb-5">Please enter your email and password!</p>
+                      <div className="form-outline form-white mb-4">
+                        <input
+                          type="password"
+                          id="password"
+                          className="form-control form-control-lg"
+                          placeholder="Password"
+                          required
+                          onChange={(e) => {
+                            setPassword(e.target.value);
+                          }}
+                        />
+                        <label className="form-label" htmlFor="typePasswordX">
+                          Password
+                        </label>
+                      </div>
 
-                                            <div className="form-outline form-white mb-4">
-                                                <input type="email" id="email" className="form-control form-control-lg" placeholder="abc@gmail.com" required onChange={(e)=>{
-                                                    setEmail(e.target.value);
-                                                }}/>
-                                                <label className="form-label" htmlFor="typeEmailX">Email</label>
-                                            </div>
+                      <div>
+                        <LoadCanvasTemplate />
+                        <input
+                          type="text"
+                          placeholder="Captcha Text"
+                          onChange={(e) => {
+                            setCaptchaText(e.target.value);
+                          }}
+                          value={captchaText}
+                          required
+                        />
+                      </div>
+                      <br />
 
-                                            <div className="form-outline form-white mb-4">
-                                                <input type="password" id="password" className="form-control form-control-lg" placeholder="Password" required onChange={(e)=>{
-                                                    setPassword(e.target.value);
-                                                }}/>
-                                                <label className="form-label" htmlFor="typePasswordX">Password</label>
-                                            </div>
-
-                                            <button className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
-
-                                        </div>
-
-                                        <div>
-                                            <p className="mb-0">Don't have an account? <a href="/buyersignup" className="text-white-50 fw-bold">Sign Up</a>
-                                            </p>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                      <button
+                        className="btn btn-outline-light btn-lg px-5"
+                        type="submit"
+                      >
+                        Login
+                      </button>
                     </div>
-                </section>
-            </form>
 
-
-
-
-
-        </div>
-    )
+                    <div>
+                      <p className="mb-0">
+                        Don't have an account?{" "}
+                        <a
+                          href="/buyersignup"
+                          className="text-white-50 fw-bold"
+                        >
+                          Sign Up
+                        </a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </form>
+    </div>
+  );
 }
