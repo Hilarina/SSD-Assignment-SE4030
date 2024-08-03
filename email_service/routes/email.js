f43fd076520bce422657a70add3f3cb024d72ba5
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-08-03 16:29:01 +0530

    Fixed issue

--- email_service/routes/email.js
+++ email_service/routes/email.js
@@ -1,17 +1,17 @@
 const router = require("express").Router();
-const sgMail = require('@sendgrid/mail')
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-router.route("/register/:name/:email").post((req, res)=>{
-    let name = req.params.name;
-    let email = req.params.email;
+router.route("/register/:name/:email").post((req, res) => {
+  let name = req.params.name;
+  let email = req.params.email;
 
-    const msg = {
-        to: email,
-        from: 'spshayurvedicbusiness@gmail.com',
-        subject: 'Registration Successful',
-        text: 'test',
-        html: `<p>
+  const msg = {
+    to: email,
+    from: "spshayurvedicbusiness@gmail.com",
+    subject: "Registration Successful",
+    text: "test",
+    html: `<p>
                 Dear ${name},<br/><br/>
 
                 Thank you for registering with us. Your account has been created successfully.<br/><br/>
@@ -24,28 +24,31 @@ router.route("/register/:name/:email").post((req, res)=>{
                 Administrator, <br/>
                 SPSH Ayurvedic Center, Sri Lanka
             </p>`,
-    }
-    sgMail.send(msg).then(() => {
-        console.log('Email sent');
-        res.send("Email sent");
-    }).catch((error) => {
-        console.error(error);
-        res.send(error);
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+      res.send("Email sent");
     })
-})
+    .catch((error) => {
+      console.error(error);
+      res.send(error);
+    });
+});
 
-router.route("/update/:name/:email").post((req, res)=>{
-    let name = req.params.name;
-    let email = req.params.email;
+router.route("/update/:name/:email").post((req, res) => {
+  let name = req.params.name;
+  let email = req.params.email;
 
-    const sgMail = require('@sendgrid/mail')
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-        const msg = {
-            to: email,
-            from: 'spshayurvedicbusiness@gmail.com',
-            subject: 'Your Account Details Have Been Updated',
-            text: 'test',
-            html: `<p>
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: email,
+    from: "spshayurvedicbusiness@gmail.com",
+    subject: "Your Account Details Have Been Updated",
+    text: "test",
+    html: `<p>
                     Dear ${name},<br/><br/>
 
                     We wanted to let you know that we have recently updated your account details. 
@@ -59,28 +62,31 @@ router.route("/update/:name/:email").post((req, res)=>{
                     Administrator, <br/>
                     SPSH Ayurvedic Center, Sri Lanka
                 </p>`,
-        }
-        sgMail.send(msg).then(() => {
-            console.log('Email sent');
-            res.send("Email sent");
-        }).catch((error) => {
-            console.error(error);
-            res.send(error);
-        })
-})
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+      res.send("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send(error);
+    });
+});
 
-router.route("/delete/:name/:email").post((req, res)=>{
-    let name = req.params.name;
-    let email = req.params.email;
+router.route("/delete/:name/:email").post((req, res) => {
+  let name = req.params.name;
+  let email = req.params.email;
 
-    const sgMail = require('@sendgrid/mail')
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-        const msg = {
-            to: email,
-            from: 'spshayurvedicbusiness@gmail.com',
-            subject: 'Your Account Has Been Removed',
-            text: 'test',
-            html: `<p>
+  const sgMail = require("@sendgrid/mail");
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: email,
+    from: "spshayurvedicbusiness@gmail.com",
+    subject: "Your Account Has Been Removed",
+    text: "test",
+    html: `<p>
                     Dear ${name},<br/><br/>
 
                     We're sorry to see you go, but we wanted to confirm that your account has been successfully removed from our system.<br/><br/>
@@ -93,35 +99,40 @@ router.route("/delete/:name/:email").post((req, res)=>{
                     Administrator, <br/>
                     SPSH Ayurvedic Center, Sri Lanka
                 </p>`,
-        }
-        sgMail.send(msg).then(() => {
-            console.log('Email sent');
-            res.send("Email sent");
-        }).catch((error) => {
-            console.error(error);
-            res.send(error);
-        })
-})
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+      res.send("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+      res.send(error);
+    });
+});
 
-router.route("/payment").post((req, res)=>{
-    let name = req.body.name;
-    let email = req.body.email;
-    let orderRef = req.body.orderRef;
-    let totAmount = req.body.totalAmount;
+router.route("/payment").post((req, res) => {
+  let name = req.body.name;
+  let email = req.body.email;
+  let orderRef = req.body.orderRef;
+  let totAmount = req.body.totalAmount;
 
-    const msg = {
-        to: email,
-        from: 'spshayurvedicbusiness@gmail.com',
-        subject: `Order Confirmation - ${orderRef}`,
-        text: 'test',
-        html: `<p>
+  const msg = {
+    to: email,
+    from: "spshayurvedicbusiness@gmail.com",
+    subject: `Order Confirmation - ${orderRef}`,
+    text: "test",
+    html: `<p>
                 Dear ${name},<br/><br/>
 
                 Thank you for your recent purchase on SPSH Ayurvedic Center. We are pleased to confirm that your order has been successfully processed and is now being prepared for shipping.<br/><br/>
 
                 <u>Order Details:</u><br/><br/>
                 <b>Order Number:</b> ${orderRef}<br/>
-                <b>Total Amount:</b> Rs.${parseFloat(totAmount).toFixed(2)}<br/><br/>
+                <b>Total Amount:</b> Rs.${parseFloat(totAmount).toFixed(
+                  2
+                )}<br/><br/>
 
                 If you have any questions or concerns, please don't hesitate to contact us. Our team is always here to help.<br/>
 
@@ -131,15 +142,18 @@ router.route("/payment").post((req, res)=>{
                 Administrator, <br/>
                 SPSH Ayurvedic Center, Sri Lanka
             </p>`,
-    }
-    sgMail.send(msg).then(() => {
-        console.log('Email sent');
-        res.send("Email sent");
-    }).catch((error) => {
-        console.error(error);
-        res.send(error);
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+      res.send("Email sent");
     })
-})
+    .catch((error) => {
+      console.error(error);
+      res.send(error);
+    });
+});
 
 // // module.exports = router;
 // const router = require("express").Router();
@@ -285,4 +299,4 @@ router.route("/payment").post((req, res)=>{
 //     }
 // });
 
-// module.exports = router;
+module.exports = router;
