e783607f7fc4f37e2f6473d00414093def279c15
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-09-14 21:12:09 +0530

    Added middleware to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,85 +1,139 @@
-import './App.css';
-import { BrowserRouter as Router, Route, } from "react-router-dom";
-import Header from './components/Header';
-import Welcome from './components/Welcome';
-import LoginBuyer from './components/LoginBuyer';
-import LoginSeller from './components/LoginSeller';
-import LoginAdmin from './components/LoginAdmin';
-import HomeAdmin from './components/HomeAdmin';
-import HomeBuyer from './components/HomeBuyer';
-import HomeSeller from './components/HomeSeller';
-import SignupBuyer from './components/SignupBuyer';
-import ManageBuyers from './components/ManageBuyers';
-import ManageSellers from './components/ManageSellers';
-import AddSeller from './components/AddSeller';
-import ViewBuyer from './components/ViewBuyer';
-import UpdateBuyer from './components/UpdateBuyer';
-import ViewSeller from './components/ViewSeller';
-import UpdateSeller from './components/UpdateSeller';
-import BuyerCart from './components/BuyerCart';
-import {PayPalScriptProvider} from "@paypal/react-paypal-js";
-import PaypalCheckoutButton from './components/PaypalCheckoutButton';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import LoginBuyer from "./components/LoginBuyer";
+import LoginSeller from "./components/LoginSeller";
+import LoginAdmin from "./components/LoginAdmin";
+import HomeAdmin from "./components/HomeAdmin";
+import HomeBuyer from "./components/HomeBuyer";
+import HomeSeller from "./components/HomeSeller";
+import SignupBuyer from "./components/SignupBuyer";
+import ManageBuyers from "./components/ManageBuyers";
+import ManageSellers from "./components/ManageSellers";
+import AddSeller from "./components/AddSeller";
+import ViewBuyer from "./components/ViewBuyer";
+import UpdateBuyer from "./components/UpdateBuyer";
+import ViewSeller from "./components/ViewSeller";
+import UpdateSeller from "./components/UpdateSeller";
+import BuyerCart from "./components/BuyerCart";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import PaypalCheckoutButton from "./components/PaypalCheckoutButton";
 
-import AddItem from './components/AddItem';
-import DeleteItem from './components/DeleteItem';
-import AllItems from './components/AllItems';
-import UpdateItem from './components/UpdateItem';
-import BuyerViewItem from './components/BuyerViewItem';
-import SingleItem from './components/SingleItem';
-import ManageOrders from './components/ManageOrders';
-import Checkout from './components/Checkout';
-import MyOrders from './components/MyOrders';
-import SingleOrder from './components/SingleOrder';
-import SingleOrderAdmin from './components/SingleOrderAdmin';
-import DeliveryRequests from './components/DeliveryRequests';
-import SingleOrderSeller from './components/SingleOrderSeller';
-
+import AddItem from "./components/AddItem";
+import DeleteItem from "./components/DeleteItem";
+import AllItems from "./components/AllItems";
+import UpdateItem from "./components/UpdateItem";
+import BuyerViewItem from "./components/BuyerViewItem";
+import SingleItem from "./components/SingleItem";
+import ManageOrders from "./components/ManageOrders";
+import Checkout from "./components/Checkout";
+import MyOrders from "./components/MyOrders";
+import SingleOrder from "./components/SingleOrder";
+import SingleOrderAdmin from "./components/SingleOrderAdmin";
+import DeliveryRequests from "./components/DeliveryRequests";
+import SingleOrderSeller from "./components/SingleOrderSeller";
+import OAuthMiddlewareBuyer from "./components/OAuthMiddlewareBuyer";
+import OAuthMiddlewareSeller from "./components/OAuthMiddlewareSeller";
 
 function App() {
   return (
-    <PayPalScriptProvider options={{"client-id": process.env.REACT_APP_CLIENT_ID}}>
-    <Router>
-      <Header/>
-      <Route path = '/' exact component = {Welcome}/>
-      <Route path = '/buyerlogin' exact component = {LoginBuyer}/>
-      <Route path = '/sellerlogin' exact component = {LoginSeller}/>
-      <Route path = '/adminlogin' exact component = {LoginAdmin}/>
+    <PayPalScriptProvider
+      options={{ "client-id": process.env.REACT_APP_CLIENT_ID }}
+    >
+      <Router>
+        <Header />
+        <Route path="/" exact component={Welcome} />
+        <Route path="/buyerlogin" exact component={LoginBuyer} />
+        <Route path="/sellerlogin" exact component={LoginSeller} />
+        <Route path="/adminlogin" exact component={LoginAdmin} />
 
-      <Route path = '/adminhome' exact component={HomeAdmin}/>
-      <Route path = '/buyerhome' exact component={HomeBuyer}/>
-      <Route path = '/sellerhome' exact component={HomeSeller}/>
-      <Route path = '/buyer/view/item/:id' exact component={BuyerViewItem}/>
-      <Route path = '/buyer/view/cart' exact component={BuyerCart}/>
-      <Route path = '/buyer/view/cart/checkout' exact component={Checkout}/>
+        <Route path="/buyeroauth" exact component={OAuthMiddlewareBuyer} />
+        <Route path="/selleroauth" exact component={OAuthMiddlewareSeller} />
 
-      <Route path = '/buyerhome/myorders' exact component={MyOrders}/>
-      <Route path = '/buyerhome/myorders/:orderRef' exact component={SingleOrder}/>
+        <Route path="/adminhome" exact component={HomeAdmin} />
+        <Route path="/buyerhome" exact component={HomeBuyer} />
+        <Route path="/sellerhome" exact component={HomeSeller} />
+        <Route path="/buyer/view/item/:id" exact component={BuyerViewItem} />
+        <Route path="/buyer/view/cart" exact component={BuyerCart} />
+        <Route path="/buyer/view/cart/checkout" exact component={Checkout} />
 
-      <Route path='/buyersignup' exact component={SignupBuyer}/>
+        <Route path="/buyerhome/myorders" exact component={MyOrders} />
+        <Route
+          path="/buyerhome/myorders/:orderRef"
+          exact
+          component={SingleOrder}
+        />
 
-      <Route path = '/adminhome/managebuyers' exact component={ManageBuyers}/>
-      <Route path = '/adminhome/managesellers' exact component={ManageSellers}/>
-      <Route path = '/adminhome/manageorders' exact component={ManageOrders}/>
-      <Route path = '/adminhome/manageorders/:orderRef' exact component={SingleOrderAdmin}/>
+        <Route path="/buyersignup" exact component={SignupBuyer} />
 
-      <Route path = '/adminhome/managesellers/add' exact component={AddSeller}/>
+        <Route path="/adminhome/managebuyers" exact component={ManageBuyers} />
+        <Route
+          path="/adminhome/managesellers"
+          exact
+          component={ManageSellers}
+        />
+        <Route path="/adminhome/manageorders" exact component={ManageOrders} />
+        <Route
+          path="/adminhome/manageorders/:orderRef"
+          exact
+          component={SingleOrderAdmin}
+        />
 
-      <Route path = '/adminhome/managebuyers/view/:email' exact component={ViewBuyer}/>
-      <Route path = '/adminhome/managebuyers/update/:paramemail' exact component={UpdateBuyer}/>
+        <Route
+          path="/adminhome/managesellers/add"
+          exact
+          component={AddSeller}
+        />
 
-      <Route path = '/adminhome/managesellers/view/:email' exact component={ViewSeller}/>
-      <Route path = '/adminhome/managesellers/update/:paramemail' exact component={UpdateSeller}/>
-      
-      <Route path='/paypal/button' exact component={PaypalCheckoutButton}/>
-    
-      <Route path ="/sellerhome/item/add/" exact component={AddItem}></Route>
-      <Route path ="/sellerhome/item" exact component={AllItems}></Route>
-      <Route path ="/sellerhome/item/delete/:id" exact componenet={DeleteItem}></Route>
-      <Route path ="/sellerhome/item/update/:id" exact component={UpdateItem}></Route>
-      <Route path ="/sellerhome/item/get/:id" exact component={SingleItem}></Route>
-      <Route path = "/sellerhome/delivery" exact component={DeliveryRequests}/>
-      <Route path = "/sellerhome/delivery/:orderRef" exact component={SingleOrderSeller}/>
-    </Router>
+        <Route
+          path="/adminhome/managebuyers/view/:email"
+          exact
+          component={ViewBuyer}
+        />
+        <Route
+          path="/adminhome/managebuyers/update/:paramemail"
+          exact
+          component={UpdateBuyer}
+        />
+
+        <Route
+          path="/adminhome/managesellers/view/:email"
+          exact
+          component={ViewSeller}
+        />
+        <Route
+          path="/adminhome/managesellers/update/:paramemail"
+          exact
+          component={UpdateSeller}
+        />
+
+        <Route path="/paypal/button" exact component={PaypalCheckoutButton} />
+
+        <Route path="/sellerhome/item/add/" exact component={AddItem}></Route>
+        <Route path="/sellerhome/item" exact component={AllItems}></Route>
+        <Route
+          path="/sellerhome/item/delete/:id"
+          exact
+          componenet={DeleteItem}
+        ></Route>
+        <Route
+          path="/sellerhome/item/update/:id"
+          exact
+          component={UpdateItem}
+        ></Route>
+        <Route
+          path="/sellerhome/item/get/:id"
+          exact
+          component={SingleItem}
+        ></Route>
+        <Route path="/sellerhome/delivery" exact component={DeliveryRequests} />
+        <Route
+          path="/sellerhome/delivery/:orderRef"
+          exact
+          component={SingleOrderSeller}
+        />
+      </Router>
     </PayPalScriptProvider>
   );
 }
