d7910e751e8b5116c44dfacb9e83c133ed5cd2f6
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2024-09-14 20:57:23 +0530

    cleared session storage

--- frontend/src/components/HomeAdmin.js
+++ frontend/src/components/HomeAdmin.js
@@ -1,44 +1,94 @@
-import Button from 'react-bootstrap/Button';
-export default function HomeAdmin(){
+import Button from "react-bootstrap/Button";
+export default function HomeAdmin() {
+  if (sessionStorage.getItem("sAyurCenNimda") === null) {
+    window.location.replace("/adminlogin");
+  }
 
-    if(sessionStorage.getItem("sAyurCenNimda") === null){
-        window.location.replace("/adminlogin");
-    }
+  return (
+    <div className="container">
+      <a
+        href="/"
+        style={{ float: "right" }}
+        onClick={() => {
+          sessionStorage.removeItem("sAyurCenNimda");
+          sessionStorage.clear();
+        }}
+      >
+        <Button variant="danger">Signout</Button>{" "}
+      </a>
 
-    return(
-        <div className="container">
-            <a href="/" style={{float: "right"}} onClick={()=>{
-                sessionStorage.removeItem("sAyurCenNimda");
-            }}>
-                <Button variant="danger">Signout</Button> </a>
-                
-    <br></br>
-                <h1 align="center" style={{fontSize: '36px',justifyContent: 'center'}}>Welcome to Admin Home</h1>
-  
-  <br></br><br></br>
-    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center',  height: '100vh'}}>
-    
-    <a href="/adminhome/managebuyers">
-      <button style={{margin: '10px', padding: '20px 40px', backgroundColor: '#007bff', color: 'white', borderColor: '#007bff', borderWidth: '1px', borderStyle: 'solid', borderRadius: '5px', fontSize: '28px', height: '350px'}}>
-        Manage Buyers
-      </button>
-    </a>
-    <a href="/adminhome/managesellers">
-      <button style={{margin: '10px', padding: '20px 40px', backgroundColor: '#28a745', color: 'white', borderColor: '#28a745', borderWidth: '1px', borderStyle: 'solid', borderRadius: '5px', fontSize: '28px', height: '350px'}}>
-        Manage Sellers
-      </button>
-    </a>
-    <a href="/adminhome/manageorders">
-      <button style={{margin: '10px', padding: '20px 40px', backgroundColor: '#fd7e14', color: 'white', borderColor: '#fd7e14', borderWidth: '1px', borderStyle: 'solid', borderRadius: '5px', fontSize: '28px', height: '350px'}}>
-        Manage Orders
-      </button>
-    </a>
-</div>
+      <br></br>
+      <h1 align="center" style={{ fontSize: "36px", justifyContent: "center" }}>
+        Welcome to Admin Home
+      </h1>
 
+      <br></br>
+      <br></br>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          height: "100vh",
+        }}
+      >
+        <a href="/adminhome/managebuyers">
+          <button
+            style={{
+              margin: "10px",
+              padding: "20px 40px",
+              backgroundColor: "#007bff",
+              color: "white",
+              borderColor: "#007bff",
+              borderWidth: "1px",
+              borderStyle: "solid",
+              borderRadius: "5px",
+              fontSize: "28px",
+              height: "350px",
+            }}
+          >
+            Manage Buyers
+          </button>
+        </a>
+        <a href="/adminhome/managesellers">
+          <button
+            style={{
+              margin: "10px",
+              padding: "20px 40px",
+              backgroundColor: "#28a745",
+              color: "white",
+              borderColor: "#28a745",
+              borderWidth: "1px",
+              borderStyle: "solid",
+              borderRadius: "5px",
+              fontSize: "28px",
+              height: "350px",
+            }}
+          >
+            Manage Sellers
+          </button>
+        </a>
+        <a href="/adminhome/manageorders">
+          <button
+            style={{
+              margin: "10px",
+              padding: "20px 40px",
+              backgroundColor: "#fd7e14",
+              color: "white",
+              borderColor: "#fd7e14",
+              borderWidth: "1px",
+              borderStyle: "solid",
+              borderRadius: "5px",
+              fontSize: "28px",
+              height: "350px",
+            }}
+          >
+            Manage Orders
+          </button>
+        </a>
+      </div>
 
-
-
-                {/* <button className="btn btn-outline-danger">Signout</button></a>
+      {/* <button className="btn btn-outline-danger">Signout</button></a>
 
              <h1 align="Center">Welcome to Admin Home</h1> 
            
@@ -46,8 +96,6 @@ export default function HomeAdmin(){
             <a href="/adminhome/managebuyers"><button>Manage Buyers</button></a> 
             <a href="/adminhome/managesellers"><button>Manage Sellers</button></a>
             <a href="/adminhome/manageorders"><button>Manage Orders</button></a>   */}
-            
-        </div>
-      
-    )
+    </div>
+  );
 }
--- frontend/src/components/HomeSeller.js
+++ frontend/src/components/HomeSeller.js
@@ -1,40 +1,83 @@
-export default function HomeSeller(){
+export default function HomeSeller() {
+  if (sessionStorage.getItem("sAyurCenRelles") === null) {
+    window.location.replace("/sellerlogin");
+  }
 
-    if(sessionStorage.getItem("sAyurCenRelles") === null){
-        window.location.replace("/sellerlogin");
-    }
-
-    return(
-        <div>
-            <a href="/" style={{float: "right"}} onClick={()=>{
-                sessionStorage.removeItem("sAyurCenRelles");
-                sessionStorage.removeItem("sellerEmail");
-            }}>
-            <button className="btn btn-danger" style={{ marginRight: '10px' }}>Signout</button>
-        </a>
-        <div align="center" style={{ border: '1px solid black', borderRadius: '5px', padding: '20px', maxWidth: '500px', margin: '0 auto', background: '#f8f9fa' }}>
+  return (
+    <div>
+      <a
+        href="/"
+        style={{ float: "right" }}
+        onClick={() => {
+          sessionStorage.removeItem("sAyurCenRelles");
+          sessionStorage.removeItem("sellerEmail");
+          sessionStorage.clear();
+        }}
+      >
+        <button className="btn btn-danger" style={{ marginRight: "10px" }}>
+          Signout
+        </button>
+      </a>
+      <div
+        align="center"
+        style={{
+          border: "1px solid black",
+          borderRadius: "5px",
+          padding: "20px",
+          maxWidth: "500px",
+          margin: "0 auto",
+          background: "#f8f9fa",
+        }}
+      >
         <h1 className="text-center">Seller Home</h1>
-        <div style={{ display: 'flex', justifyContent: 'center' }}></div>
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <div align="center" style={{ border: '1px solid black', borderRadius: '5px', padding: '20px', maxWidth: '500px', background: '#f8f9fa' }}>  
-                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                    <a href="/sellerhome/item">
-                        <button className="btn btn-primary btn-lg" style={{ width: '200px', height: '200px', backgroundColor: 'blue', marginRight: '10px' }}>
-                        Manage Items
-                        </button>
-                    </a>
-                    <a href="/sellerhome/delivery">
-                        <button className="btn btn-secondary btn-lg" style={{ width: '200px', height: '200px', backgroundColor: 'green' }}>
-                        Delivery Requests
-                        </button>
-                    </a>
-                </div>
+        <div style={{ display: "flex", justifyContent: "center" }}></div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            align="center"
+            style={{
+              border: "1px solid black",
+              borderRadius: "5px",
+              padding: "20px",
+              maxWidth: "500px",
+              background: "#f8f9fa",
+            }}
+          >
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+              <a href="/sellerhome/item">
+                <button
+                  className="btn btn-primary btn-lg"
+                  style={{
+                    width: "200px",
+                    height: "200px",
+                    backgroundColor: "blue",
+                    marginRight: "10px",
+                  }}
+                >
+                  Manage Items
+                </button>
+              </a>
+              <a href="/sellerhome/delivery">
+                <button
+                  className="btn btn-secondary btn-lg"
+                  style={{
+                    width: "200px",
+                    height: "200px",
+                    backgroundColor: "green",
+                  }}
+                >
+                  Delivery Requests
+                </button>
+              </a>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-
-
-        
-    )
+  );
 }
