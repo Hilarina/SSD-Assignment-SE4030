9445fd9370c4a7b61487fdebfca23bb6a3ee8a25
Sathira-Wijeratne <swije27@gmail.com>
2024-09-16 16:51:34 +0530

    implemented csrf changes to buyerviewitems

--- frontend/src/components/BuyerViewItem.js
+++ frontend/src/components/BuyerViewItem.js
@@ -23,6 +23,7 @@ export default function BuyerViewItem() {
     const [MaxQuantity, setMaxQuantity] = useState(); // Maximum quantity of the product available in stock
     const [Image, setImage] = useState(""); // Base64 encoded string of the product image
     const [rate, setRate] = useState(0); // Initial user rating for the product is set to 0
+    const [csrfToken, setCsrfToken] = useState("");
 
     // Getting the product ID from the URL using useParams hook
     const { id } = useParams();
@@ -30,6 +31,12 @@ export default function BuyerViewItem() {
     const buyerEmail = sessionStorage.getItem("buyerEmail"); //implement this to get the buyer email from sessions
 
     const [newRemainingQty, setNewRemainingQty] = useState();
+
+    useEffect(()=>{
+        axios.get('http://localhost:8070/csrf-token', {withCredentials:true})
+        .then((res)=> setCsrfToken(res.data.csrfToken))
+        .catch((err)=>console.error("Error fetching CSRF token"));
+    }, []);
 
     useEffect(() => {
         // Fetching the product data from the database
@@ -77,7 +84,9 @@ export default function BuyerViewItem() {
         } else if(Quantity > MaxQuantity){
             alert("Insufficient Quantity!");
         } else {
-            axios.post(`http://localhost:8070/ShoppingCart/add`, newCart).then(() => {
+            axios.post(`http://localhost:8070/ShoppingCart/add`, newCart, {headers:{
+                "CSRF-Token":csrfToken,
+            },withCredentials: true}).then(() => {
                 updateItem(newRemainingQty);
                 alert("Item added to cart");
                 window.location.replace("http://localhost:3000/buyerhome");
